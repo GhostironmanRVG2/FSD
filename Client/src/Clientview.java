ad40122ac4e9bc3a0688e9b270d5169bf200ce39
GhostironmanRVG2 <GhostironmanRVG2@gmail.com>
2021-12-10 19:26:59 +0000

    10/12/2021

--- Client/src/Clientview.java
+++ Client/src/Clientview.java
@@ -1,3 +1,24 @@
+import java.awt.Color;
+import java.awt.event.*; 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import views.*;
+
 public class Clientview {
     
+
+
+ public static void main(String[] args) {  
+   JFrame janela=new JFrame();
+ }
+
+
+
+
+
+
+
+
+
 }
